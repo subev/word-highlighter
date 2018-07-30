699b0847c54972fc695a3838e665ac7e17e78377
Petur Subev <petur.subev@gmail.com>
2018-07-31 02:28:31 +0300

    Add prototype for word highlighter

--- chrome/extension/inject.js
+++ chrome/extension/inject.js
@@ -1,48 +1,64 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Dock from 'react-dock';
+import Mark from 'mark.js';
+import { fromEvent } from 'rxjs';
+import { filter, map, mapTo, mergeMap, concatMap, first, last, takeUntil } from 'rxjs/operators';
 
-class InjectApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isVisible: false };
+const log = (x) => {
+  console.log(x, new Date());
+  return x;
+};
+
+const getRange = () => window.getSelection().getRangeAt(0);
+const escapeRegExp = text => text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+
+const restoreSelection = (range) => {
+  const sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+};
+
+const isSameContainer = range => (
+  (range.startContainer === range.commonAncestorContainer)
+    && (range.endContainer === range.commonAncestorContainer)
+);
+
+const instance = new Mark(document.querySelectorAll('div,a,span,p,td,code'));
+const clearMark = () => instance.unmark({});
+
+const highlight = () => {
+  const savedRange = getRange();
+  if (!isSameContainer(savedRange)) {
+    // complex ranges are not supported
+    return;
   }
+  const selectedText = window.getSelection().toString().trim();
+  console.log('HIGHLIGHT', selectedText);
+  clearMark();
+  instance.markRegExp(new RegExp(escapeRegExp(selectedText), 'g'), {
+    iframes: false,
+    debug: true
+  });
+  restoreSelection(savedRange);
+};
 
-  buttonOnClick = () => {
-    this.setState({ isVisible: !this.state.isVisible });
-  };
+fromEvent(document, 'dblclick')
+  .pipe(filter(({ target }) => !(target instanceof HTMLInputElement)))
+  .subscribe(highlight);
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.buttonOnClick}>
-          Open TodoApp
-        </button>
-        <Dock
-          position="right"
-          dimMode="transparent"
-          defaultSize={0.4}
-          isVisible={this.state.isVisible}
-        >
-          <iframe
-            style={{
-              width: '100%',
-              height: '100%',
-            }}
-            frameBorder={0}
-            allowTransparency="true"
-            src={chrome.extension.getURL(`inject.html?protocol=${location.protocol}`)}
-          />
-        </Dock>
-      </div>
-    );
-  }
-}
+fromEvent(document, 'keyup')
+  .pipe(filter(me => me.keyCode === 27))
+  .subscribe(clearMark);
 
-window.addEventListener('load', () => {
-  const injectDOM = document.createElement('div');
-  injectDOM.className = 'inject-react-example';
-  injectDOM.style.textAlign = 'center';
-  document.body.appendChild(injectDOM);
-  render(<InjectApp />, injectDOM);
-});
+fromEvent(document, 'selectionchange')
+  .pipe(
+    // normal clicks that have no selection are also considered selections
+    filter(() => window.getSelection().toString()),
+    concatMap(sc =>
+      fromEvent(document, 'mouseup')
+      .pipe(first(), mapTo(sc))
+    )
+  )
+  .pipe(
+    map(getRange),
+    filter(isSameContainer),
+  )
+  .subscribe(highlight);
